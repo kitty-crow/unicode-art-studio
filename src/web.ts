90c27fd21d56c26aa86f2a827825b06c55fcae9b
kitty <141717449+kitty-crow@users.noreply.github.com>
2026-09-06 14:53:25 +0100

    feat: initialise animation studio

--- src/web.ts
+++ src/web.ts
@@ -1,5 +1,7 @@
+import { startAnimationStudio } from "./web/animation.ts";
 import { bindPresetPreferences } from "./web/preset-preferences.ts";
 import { startStudio } from "./web/studio.ts";
 
 startStudio();
+startAnimationStudio();
 bindPresetPreferences();
